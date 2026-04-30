7bb18cde61a42924d4d7d96fe2e53d0371f32465
jlcorrei <103605432+jlcorrei@users.noreply.github.com>
2026-04-30 13:38:42 -0400

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -43,9 +43,129 @@ wget https://huggingface.co/datasets/Csc722Sp26Group1/CSC722_SP26_GROUP1_CHANGE_
 wget https://huggingface.co/nvidia/MambaVision-B-1K/resolve/main/mambavision_base_1k.pth.tar
 cd ../
 
-echo "Copying over checkpoint..."
-cp -r /mnt/ncsudrive/j/jlcorrei/checkpoints/hyret_whu_swinb/ checkpoints/
-cp -r /mnt/ncsudrive/j/jlcorrei/checkpoints/hyret_whu_swinb_cityscapes/ checkpoints/
+echo "Copying over author's checkpoints..."
+cd checkpoints/
+mkdir hyret_levir
+cd hyret_levir
+wget https://huggingface.co/mustansarfiaz/HyRet/resolve/main/hyret_levir_ckpt.pt
+cd ../
+mkdir hyret_cdd
+cd hyret_cdd
+wget https://huggingface.co/mustansarfiaz/HyRet/resolve/main/hyret_cdd_ckpt.pt
+cd ../
+mkdir hyret_whu
+cd hyret_whu
+wget https://huggingface.co/mustansarfiaz/HyRet/resolve/main/hyret_whu_ckpt.pt
+cd ../
+
+echo "Copying over student checkpoints..."
+# Please uncomment the commands for the dataset you'd like to evaluate:
+
+# LEVIR
+mkdir hyret_levir_resnet101
+cd hyret_levir_resnet101
+wget https://huggingface.co/datasets/Csc722Sp26Group1/CSC722_SP26_GROUP1_CHANGE_DETECTION/resolve/main/hyret_levir_resnet101_ckpt.pt
+cd ../
+mkdir hyret_levir_swint
+cd hyret_levir_swint
+wget https://huggingface.co/datasets/Csc722Sp26Group1/CSC722_SP26_GROUP1_CHANGE_DETECTION/resolve/main/hyret_levir_swint_ckpt.pt
+cd ../
+mkdir hyret_levir_swint_citysem
+cd hyret_levir_swint_citysem
+wget https://huggingface.co/datasets/Csc722Sp26Group1/CSC722_SP26_GROUP1_CHANGE_DETECTION/resolve/main/hyret_levir_swint_citysem_ckpt.pt
+cd ../
+wget https://huggingface.co/datasets/Csc722Sp26Group1/CSC722_SP26_GROUP1_CHANGE_DETECTION/resolve/main/hyret_levir_swinb_ckpt.pt
+mkdir hyret_levir_swinb_citysem
+cd hyret_levir_swinb_citysem
+wget https://huggingface.co/datasets/Csc722Sp26Group1/CSC722_SP26_GROUP1_CHANGE_DETECTION/resolve/main/hyret_levir_swinb_citysem_ckpt.pt
+cd ../
+mkdir hyret_levir_mamba1k
+cd hyret_levir_mamba1k
+wget https://huggingface.co/datasets/Csc722Sp26Group1/CSC722_SP26_GROUP1_CHANGE_DETECTION/resolve/main/hyret_levir_mamba1k_ckpt.pt
+cd ../
+mkdir hyret_levir_resnet50_flip30
+cd hyret_levir_resnet50_flip30
+wget https://huggingface.co/datasets/Csc722Sp26Group1/CSC722_SP26_GROUP1_CHANGE_DETECTION/resolve/main/hyret_levir_resnet50_flip30_ckpt.pt
+cd ../
+mkdir hyret_levir_resnet50_flip30_crop
+cd hyret_levir_resnet50_flip30_crop
+wget https://huggingface.co/datasets/Csc722Sp26Group1/CSC722_SP26_GROUP1_CHANGE_DETECTION/resolve/main/hyret_levir_resnet50_flip30_crop_ckpt.pt
+cd ../
+mkdir hyret_levir_resnet50_flip30_crop_ce_dice
+cd hyret_levir_resnet50_flip30_crop_ce_dice
+wget https://huggingface.co/datasets/Csc722Sp26Group1/CSC722_SP26_GROUP1_CHANGE_DETECTION/resolve/main/hyret_levir_resnet50_flip30_crop_ce_dice_ckpt.pt
+cd ../
+
+# CDD
+# mkdir hyret_cdd_resnet101
+# cd hyret_cdd_resnet101
+# wget https://huggingface.co/datasets/Csc722Sp26Group1/CSC722_SP26_GROUP1_CHANGE_DETECTION/resolve/main/hyret_cdd_resnet101_ckpt.pt
+# cd ../
+# mkdir hyret_cdd_swint
+# cd hyret_cdd_swint
+# wget https://huggingface.co/datasets/Csc722Sp26Group1/CSC722_SP26_GROUP1_CHANGE_DETECTION/resolve/main/hyret_cdd_swint_ckpt.pt
+# cd ../
+# mkdir hyret_cdd_swint_citysem
+# cd hyret_cdd_swint_citysem
+# wget https://huggingface.co/datasets/Csc722Sp26Group1/CSC722_SP26_GROUP1_CHANGE_DETECTION/resolve/main/hyret_cdd_swint_citysem_ckpt.pt
+# cd ../
+# wget https://huggingface.co/datasets/Csc722Sp26Group1/CSC722_SP26_GROUP1_CHANGE_DETECTION/resolve/main/hyret_cdd_swinb_ckpt.pt
+# mkdir hyret_cdd_swinb_citysem
+# cd hyret_cdd_swinb_citysem
+# wget https://huggingface.co/datasets/Csc722Sp26Group1/CSC722_SP26_GROUP1_CHANGE_DETECTION/resolve/main/hyret_cdd_swinb_citysem_ckpt.pt
+# cd ../
+# mkdir hyret_cdd_mamba1k
+# cd hyret_cdd_mamba1k
+# wget https://huggingface.co/datasets/Csc722Sp26Group1/CSC722_SP26_GROUP1_CHANGE_DETECTION/resolve/main/hyret_cdd_mamba1k_ckpt.pt
+# cd ../
+# mkdir hyret_cdd_resnet50_flip30
+# cd hyret_cdd_resnet50_flip30
+# wget https://huggingface.co/datasets/Csc722Sp26Group1/CSC722_SP26_GROUP1_CHANGE_DETECTION/resolve/main/hyret_cdd_resnet50_flip30_ckpt.pt
+# cd ../
+# mkdir hyret_cdd_resnet50_flip30_crop
+# cd hyret_cdd_resnet50_flip30_crop
+# wget https://huggingface.co/datasets/Csc722Sp26Group1/CSC722_SP26_GROUP1_CHANGE_DETECTION/resolve/main/hyret_cdd_resnet50_flip30_crop_ckpt.pt
+# cd ../
+# mkdir hyret_cdd_resnet50_flip30_crop_ce_dice
+# cd hyret_cdd_resnet50_flip30_crop_ce_dice
+# wget https://huggingface.co/datasets/Csc722Sp26Group1/CSC722_SP26_GROUP1_CHANGE_DETECTION/resolve/main/hyret_cdd_resnet50_flip30_crop_ce_dice_ckpt.pt
+# cd ../
+
+# WHU
+# mkdir hyret_whu_resnet101
+# cd hyret_whu_resnet101
+# wget https://huggingface.co/datasets/Csc722Sp26Group1/CSC722_SP26_GROUP1_CHANGE_DETECTION/resolve/main/hyret_whu_resnet101_ckpt.pt
+# cd ../
+# mkdir hyret_whu_swint
+# cd hyret_whu_swint
+# wget https://huggingface.co/datasets/Csc722Sp26Group1/CSC722_SP26_GROUP1_CHANGE_DETECTION/resolve/main/hyret_whu_swint_ckpt.pt
+# cd ../
+# mkdir hyret_whu_swint_citysem
+# cd hyret_whu_swint_citysem
+# wget https://huggingface.co/datasets/Csc722Sp26Group1/CSC722_SP26_GROUP1_CHANGE_DETECTION/resolve/main/hyret_whu_swint_citysem_ckpt.pt
+# cd ../
+# wget https://huggingface.co/datasets/Csc722Sp26Group1/CSC722_SP26_GROUP1_CHANGE_DETECTION/resolve/main/hyret_whu_swinb_ckpt.pt
+# mkdir hyret_whu_swinb_citysem
+# cd hyret_whu_swinb_citysem
+# wget https://huggingface.co/datasets/Csc722Sp26Group1/CSC722_SP26_GROUP1_CHANGE_DETECTION/resolve/main/hyret_whu_swinb_citysem_ckpt.pt
+# cd ../
+# mkdir hyret_whu_mamba1k
+# cd hyret_whu_mamba1k
+# wget https://huggingface.co/datasets/Csc722Sp26Group1/CSC722_SP26_GROUP1_CHANGE_DETECTION/resolve/main/hyret_whu_mamba1k_ckpt.pt
+# cd ../
+# mkdir hyret_whu_resnet50_flip30
+# cd hyret_whu_resnet50_flip30
+# wget https://huggingface.co/datasets/Csc722Sp26Group1/CSC722_SP26_GROUP1_CHANGE_DETECTION/resolve/main/hyret_whu_resnet50_flip30_ckpt.pt
+# cd ../
+# mkdir hyret_whu_resnet50_flip30_crop
+# cd hyret_whu_resnet50_flip30_crop
+# wget https://huggingface.co/datasets/Csc722Sp26Group1/CSC722_SP26_GROUP1_CHANGE_DETECTION/resolve/main/hyret_whu_resnet50_flip30_crop_ckpt.pt
+# cd ../
+# mkdir hyret_whu_resnet50_flip30_crop_ce_dice
+# cd hyret_whu_resnet50_flip30_crop_ce_dice
+# wget https://huggingface.co/datasets/Csc722Sp26Group1/CSC722_SP26_GROUP1_CHANGE_DETECTION/resolve/main/hyret_whu_resnet50_flip30_crop_ce_dice_ckpt.pt
+# cd ../
+cd ../
 
 echo "Installing dependencies..."
 python -m pip install torch==2.1.0 torchvision==0.16.0 torchaudio==2.1.0 --index-url https://download.pytorch.org/whl/cu121 
